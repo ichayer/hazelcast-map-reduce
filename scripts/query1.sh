e747ee971454c1af10295cbe23f604de6c309ae5
ichayer <ichayer@itba.edu.ar>
2023-11-01 20:00:12 -0300

    Update scripts

--- scripts/query1.sh
+++ scripts/query1.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-cd ./tmp/tpe2-g4-client-2023.2Q/ && ./query1 -Daddresses=\'192.168.1.38:5701\' -DinPath="$PWD/../csv" -DoutPath="$PWD/../.." $*
+cd ./tmp/tpe2-g4-client-2023.2Q/ && ./query1.sh -Daddresses=\'192.168.1.38:5701\' -DinPath="$PWD/../csv" -DoutPath="$PWD/../.." $*
--- scripts/query2.sh
+++ scripts/query2.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-cd ./tmp/tpe2-g4-client-2023.2Q/ && ./query2 -Daddresses=\'192.168.1.38:5701\' -DinPath="$PWD/../csv" -DoutPath="$PWD/../.." -Dn=4 $*
+cd ./tmp/tpe2-g4-client-2023.2Q/ && ./query2.sh -Daddresses=\'10.9.68.132:5701\' -DinPath="$PWD/../csv" -DoutPath="$PWD/../.." -Dn=4 $*
