142933fb6331c815be3b76e601eebda1f57901cd
ThomasMiz <32400648+ThomasMiz@users.noreply.github.com>
2023-11-02 21:06:11 -0300

    Add missing script for Query1FromStations strategy

--- scripts/query4.sh
+++ scripts/query4.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-cd ./tmp/tpe2-g4-client-2023.2Q/ && ./query4.sh -Daddresses=\'127.0.0.1:5701\' -DinPath="$PWD/../csv" -DoutPath="$PWD/../.." -DstartDate=01/05/2021 -DendDate=31/05/2021 $*
+cd ./tmp/tpe2-g4-client-2023.2Q/ && ./query4.sh -Daddresses=\'127.0.0.1:5701\' -DinPath="$PWD/../csv" -DoutPath="$PWD/../.." -DstartDate=01/01/2021 -DendDate=31/12/2021 $*
